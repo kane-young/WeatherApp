d4973cb1b7d9ff4ab56d942b1f8f655098392343
kane-young <lyw2100@naver.com>
2022-03-22 14:34:34 +0900

    [#02] City 타입 생성 및 구현

---
+++ WeatherApp/WeatherApp/Sources/City.swift
@@ -0,0 +1,25 @@
+//
+//  City.swift
+//  WeatherApp
+//
+//  Created by 이영우 on 2022/03/22.
+//
+
+import Foundation
+
+enum City {
+  case seoul
+  case london
+  case chicago
+
+  var identifier: String {
+    switch self {
+    case .seoul:
+      return "1132599"
+    case .london:
+      return "44418"
+    case .chicago:
+      return "2379574"
+    }
+  }
+}

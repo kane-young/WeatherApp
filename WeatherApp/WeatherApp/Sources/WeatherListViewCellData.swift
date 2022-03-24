7bdc410ade99a8bbba8d238fce481d0393f5ac80
kane-young <lyw2100@naver.com>
2022-03-24 16:49:16 +0900

    [#6] 날씨리스트를 보여주기 위한 cellData 생성

---
+++ WeatherApp/WeatherApp/Sources/WeatherListViewCellData.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherListViewCellData.swift
+//  WeatherApp
+//
+//  Created by 이영우 on 2022/03/24.
+//
+
+import Foundation
+
+struct WeatherListViewCellData {
+  let dateText: String
+  let weatherState: WeatherState
+  let maxTemperature: Int
+  let minTemperature: Int
+}
